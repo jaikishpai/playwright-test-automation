f84e148f1b2b33030f70f91a4c8ce2692c74000a
Jaikish Pai <jkshpai@gmail.com>
2024-12-30 23:23:00 +0100

    added more tests - update settings

--- pageobjects/landing_page.py
+++ pageobjects/landing_page.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from utility.logger import get_logger
 
 from playwright.sync_api import Page
@@ -28,34 +30,53 @@ class LandingPage:
         self.upload_speed_value = page.locator("#upload-value")
         self.upload_speed_units = page.locator("#upload-units")
         self.settings_button = page.locator("#settings-link")
-
+        self.minimum_connection_input = page.locator("#min-connections-input")
+        self.maximum_connection_input = page.locator("#max-connections-input")
+        self.measure_load_latency_checkbox = page.get_by_label("Measure loaded latency during")
+        self.always_show_metrics_checkbox =  page.get_by_label("Always show all metrics")
+        self.save_config_checkbox = page.get_by_label("Save config for this device")
+        self.save_settings_button = page.get_by_role("link", name="Save")
 
 
     def navigate_to_landing_page(self):
-        logger.info("Navigating to landing page")
+        logger.info("Navigating to landing page...")
         self.page.goto(self.landing_page)
 
     def click_show_more_info_link(self):
-        logger.info("Clicking show more info link")
+        logger.info("Clicking show more info link...")
         self.show_more_link.click()
 
     def wait_for_speedtest_completion(self):
-        logger.info("Waiting for speed test completion")
+        logger.info("Waiting for speed test completion...")
         self.show_more_link.wait_for(state="visible", timeout=60000)
 
     def get_speed(self):
-        logger.info("Getting speed value")
+        logger.info("Getting speed value...")
         self.wait_for_speedtest_completion()
         return self.speed_value.text_content(), self.speed_units.text_content()
 
     def get_upload_speed(self):
-        logger.info("Getting upload speed value")
+        logger.info("Getting upload speed value...")
         self.upload_complete_class.wait_for(state="visible", timeout=60000)
         return self.upload_speed_value.text_content(), self.upload_speed_units.text_content()
 
     def click_settings_button(self):
-        logger.info("Clicking settings button")
+        logger.info("Clicking settings button...")
         self.settings_button.click()
+
+    def update_settings(self, min_connections="2", max_connections="10"):
+        logger.info("Updating settings...")
+        logger.info("Updating minimum connections...")
+        self.minimum_connection_input.fill(min_connections)
+        logger.info("Updating maximum connections...")
+        self.maximum_connection_input.fill(max_connections)
+        logger.info("Updating checkboxes...")
+        self.measure_load_latency_checkbox.check()
+        self.always_show_metrics_checkbox.check()
+        self.save_config_checkbox.check()
+        logger.info("Clicking on Save button...")
+        self.save_settings_button.click()
+
 
 
 
--- tests/test_landing_page.py
+++ tests/test_landing_page.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from utility.logger import get_logger
 import pytest
 
@@ -20,10 +22,10 @@ logger = get_logger(__name__)
 
 @pytest.mark.regression
 def test_landing_page(page:Page):
-    logger.info("Starting Landing Page Test")
+    logger.info("Starting Landing Page Test...")
     lp = LandingPage(page)
     lp.navigate_to_landing_page()
-    logger.info("Verifying Title and Logo")
+    logger.info("Verifying Title and Logo...")
     expect(page).to_have_title("Internet Speed Test | Fast.com")
     expect(lp.logo).to_be_visible()
     speed_value, speed_units = lp.get_speed()
@@ -32,17 +34,32 @@ def test_landing_page(page:Page):
 
 
 def test_show_more_info(page:Page):
-    logger.info("Starting Show More Info Test")
+    logger.info("Starting Show More Info Test...")
     lp = LandingPage(page)
     lp.navigate_to_landing_page()
     lp.wait_for_speedtest_completion()
     lp.click_show_more_info_link()
-    logger.info("Verifying Latency Field and Upload Speed")
+    logger.info("Verifying Latency Field and Upload Speed...")
     expect(lp.latency_container).to_be_visible()
     expect(lp.upload_field).to_be_visible()
     upload_value, upload_units = lp.get_upload_speed()
     time_stamp = return_timestamp()
     logger.info(f'\n{time_stamp} :: Upload Speed :: {upload_value} {upload_units}')
+
+
+def test_update_speedtest_settings(page:Page):
+    logger.info("Starting Update Speed Test")
+    lp = LandingPage(page)
+    lp.navigate_to_landing_page()
+    lp.wait_for_speedtest_completion()
+    lp.click_show_more_info_link()
+    lp.click_settings_button()
+    lp.update_settings(min_connections="3", max_connections="20")
+
+
+
+
+
 
 
 
